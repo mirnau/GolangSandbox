0c7db443cfd771edafe0e55eb5df18156e5a7745
Mikael Bernau <mikael.bernau@gmail.com>
2024-09-05 21:49:25 +0200

    Initial commit

---
+++ algorithms/binarysearch.go
@@ -0,0 +1,29 @@
+package algorithms
+
+/*
+	Requires a sorted array as input a
+*/
+
+type Comparator[T any] func(a, b T) int
+
+func BinarySearch[T any](key T, a []T, cmp Comparator[T]) T {
+
+	low := 0
+	high := len(a) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		comparison := cmp(a[mid], key)
+		if comparison == 0 {
+			return a[mid]
+		}
+		if comparison < 0 {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	var zero T
+	return zero
+}
